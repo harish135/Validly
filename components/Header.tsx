34212a4d5300357b4b8dd87e455ea4ad092ee517
harish135 <ravulaharish113@gmail.com>
2025-05-30 11:54:58 -0500

    Update Header: small change

--- components/Header.tsx
+++ components/Header.tsx
@@ -91,9 +91,9 @@ const Header: React.FC<HeaderProps> = ({
                     className="h-8 w-8 rounded-full"
                   />
                 )}
-                <span className="text-white text-sm font-medium">
+                {/* <span className="text-white text-sm font-medium">
                   {user.name ? user.name.charAt(0).toUpperCase() : ''}
-                </span>
+                </span> */}
                 <button
                   onClick={onSignOut}
                   className="px-4 py-2 text-sm font-medium text-white bg-red-600 rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
