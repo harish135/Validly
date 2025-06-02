ca426f04b8e446424950d4ca3f67d7d855454dd2
harish135 <55907568+harish135@users.noreply.github.com>
2025-06-02 13:27:10 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -430,7 +430,7 @@ const App: React.FC = () => {
                   <FormulationAdvisorPage />
                 </ProtectedRoute>
               } />
-              <Route path="*" element={<Navigate to="/" replace />} />
+              <Route path="*" element={<Navigate to="/\" replace />} />
             </Routes>
           </main>
           <Footer navigateTo={navigateTo} />
